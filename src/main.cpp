924eba30529f628c69d21abc91dc3bfe10c3795d
T&R <bottest9@gmail.com>
2022-11-05 17:49:31 -0500

    set up local git stuff on Gaia, Added more docs

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+//Pin info, what's hooked up to what
+//currently placeholder values
+#define GPS = 4;
+#define MAG = 14;
+#define LED = 15;
+#define SCR = 27;
+
+void setup() {
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
