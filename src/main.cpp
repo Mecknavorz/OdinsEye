e32192bea8e5e0097a70955765c31294f8f43660
T&R <bottest9@gmail.com>
2022-11-06 13:53:44 -0600

    LED ring test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>  //librariers for the LED ring
 
 //Pin info, what's hooked up to what
 //currently placeholder values
-#define GPS = 4;
-#define MAG = 14;
-#define LED = 15;
-#define SCR = 27;
+#define LEDPIN 1
+
+//how many pixels we have
+#define NUMPIXELS 24
+//refresh rate of LED ring
+#define DELAY 100
+
+
+Adafruit_NeoPixel pixels(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  //setup stuff
+  pixels.begin(); //turn on our ring
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+//controls the LED ring
+//sDir is the direction of our selves
+//gDir is the direction of the geocache
+void ringControl(){
+  pixels.clear();
+  for(int i=0; i<NUMPIXELS; i++){
+    //pixel colors are r, g, b
+    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+    pixels.show();
+    delay(DELAY);
+  }
 }
