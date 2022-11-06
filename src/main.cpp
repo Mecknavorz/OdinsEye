081da991fda65b7ad21d708be85646ab03a548f0
T&R <bottest9@gmail.com>
2022-11-06 15:01:48 -0600

    LED ring test code w/FastLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,37 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>  //librariers for the LED ring
+#include <FastLED.h>  //librariers for the LED ring
 
 //Pin info, what's hooked up to what
 //currently placeholder values
 #define LEDPIN 1
 
 //how many pixels we have
-#define NUMPIXELS 24
-//refresh rate of LED ring
-#define DELAY 100
-
-
-Adafruit_NeoPixel pixels(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+//the ring has 24 but we're using the 0 as a sacrifice
+#define NUM_LEDS 25
+CRGB leds[NUM_LEDS]; //array to manage LEDs
 
 void setup() {
   //setup stuff
-  pixels.begin(); //turn on our ring
+  //LED Ring Set Up
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ringControl();
 }
 
 //controls the LED ring
 //sDir is the direction of our selves
 //gDir is the direction of the geocache
 void ringControl(){
-  pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++){
-    //pixel colors are r, g, b
-    pixels.setPixelColor(i, pixels.Color(150, 0, 0));
-    pixels.show();
-    delay(DELAY);
-  }
+  //generic blink
+  leds[0] = CRGB::White;
+  FastLED.show();
+  delay(30);
+
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(30);
 }
