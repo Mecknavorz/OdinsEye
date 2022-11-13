71f1dc810717d5a728c4192d0ad6ead119615951
T&R <bottest9@gmail.com>
2022-11-12 23:46:43 -0600

    finished basic geocache class and started fetched class

--- software_design/src/fetcher.cpp
+++ software_design/src/fetcher.cpp
@@ -2,3 +2,29 @@
 //made by T&R (Mecknavorz)
 //Fetcher is a custom setup to store and get geocache data
 //done cause i didn't wanna deal with requesting geocache API access lol
+
+//includes
+#include "Arduino.h"
+#include "fetcher.h"
+
+//defualt constructor
+geocache::geocache(){
+    lat = 0.0;
+    lon = 0.0;
+    dif = 0;
+    ter = 0;
+    size = 0;
+}
+//geocache constructor
+geocache::geocache(double Lat, double Long, int Dif, int Ter, int Siz){
+    lat = Lat;
+    lon = Long;
+    dif = Dif;
+    ter = Ter;
+    size = Siz;
+}
+
+//defualt fetcher constructor
+fetcher::fetcher(){
+    geocache stored[20];
+}
--- software_design/src/fetcher.h
+++ software_design/src/fetcher.h
@@ -2,18 +2,23 @@
 //made by T&R (Mecknavorz)
 //Fetcher is a custom setup to store and get geocache data
 //done cause i didn't wanna deal with requesting geocache API access lol
+#ifndef fetcher_h
+#define fetcher_h
 
 //basic includes
 #include "Arduino.h"
 
 class geocache{
     public:
+    //variables
     double lat; //latitude - double?
     double lon; //longitude
     int dif; //difficulty
     int ter; //terrain
-    int size;//size
-
+    int size; //size
+    //functions
+    geocache();
+    geocache(double Lat, double Long, int Dif, int Ter, int Siz); //defualt constructor
     //string type;
     //description?????
     private:
@@ -21,3 +26,12 @@ class geocache{
     //logged
     //-
 };
+
+class fetcher{
+    public:
+    geocache nearme[20]; //nearby geocaches
+    fetcher(); //defualt constructor
+    void getNearby();
+};
+
+#endif
